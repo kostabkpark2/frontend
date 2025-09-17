f3fb087c693f91479cb02d0c21edea99a801997a
kostabkpark2 <kostabkpark2@gmail.com>
2025-09-17 11:01:24 +0900

    javascript day1

---
+++ js/card.js
@@ -0,0 +1,11 @@
+ // function over() { 매개변수 없고 반환값 없는 함수
+//     obj = document.getElementById("card");
+//     obj.src='image/card_front.jpg'
+// }
+function over(obj) { //  매개변수가 있고 반환값 없는 함수
+    obj.src = './image/card_front.jpg';
+}
+
+function out(obj) { //  매개변수가 있고 반환값 없는 함수
+    obj.src = './image/card_back.png';
+}
